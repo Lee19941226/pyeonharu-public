8a3b16998f0e2de827378257e4721216e9105931
ljw155070-a11y <ljw155070@gmail.com>
2026-02-11 21:01:55 +0900

    feat : 광고 스크립트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,18 +54,18 @@ export default function RootLayout({
           as="style"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4884937144207124"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className="font-sans antialiased bg-background text-foreground">
         <Script
           strategy="beforeInteractive"
           src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}`}
         />
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4884937144207124"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+
         <AuthProvider>
           {children}
           <Toaster />
